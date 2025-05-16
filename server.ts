f119bf385d7457f95a33786475431940f3705d93
Priya <priya.saran.w3era@gmail.com>
2025-05-16 10:28:01 +0530

    set up server

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import  app from './src/app'
+import app from './src/app'
 const startServer=()=>{
     const port=process.env.PORT||3000
     app.listen(port,()=>{
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,37 @@
 import express from "express";
 const app=express()
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.json({message:'hello world'})
+})
+// app.post('/' ,(req,res)=>{
+//     res.send('hello world from post request')
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default app;
