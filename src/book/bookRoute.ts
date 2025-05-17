60770656584b6725376473b78f2500cb1ad6d698
Priya <priya.saran.w3era@gmail.com>
2025-05-17 17:42:20 +0530

    set up multer

--- src/book/bookRoute.ts
+++ src/book/bookRoute.ts
@@ -19,12 +19,22 @@ bookRouter.get("/", async (req: Request, res: Response,next:NextFunction) => {
         });
     }
 });
-const upload = multer({dest: path.join(__dirname, 'uploads')});
+const upload = multer({dest: path.resolve(__dirname, '../../public/data/uploads'),
+    limits: { fileSize: 3e7 },
+});
 
-
-
-bookRouter.post("/", (req: Request, res: Response) => {
+//here uploader will work as an middleware so it will be placed before the route handler and then the route handler will get exact prepared data 
+bookRouter.post("/",upload.fields([
+    {name:"coverImage",maxCount:1},
+    {name:"file",maxCount:1}
+]) ,(req: Request, res: Response) => {
     const {title , author , file , coverImage , genre } = req.body;
     res.json("create a new book");
 });
+
 export default bookRouter;
+
+    bookRouter.put('/',
+    (req:Request ,res:Response ,next:NextFunction)=>{
+    res.json({status : true , message: "update a book"})
+    })
